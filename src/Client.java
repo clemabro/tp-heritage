322c9df2c9c76a7bcb4a913312600002cf5ded70
Clément AZIBEIRO <clementazibeiro@sfr.fr>
2020-07-02 15:10:24 +0200

    Fin TP1

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Client {
 	private long numero;
 	private String nom;
+	private ArrayList<Compte> comptes = new ArrayList<Compte>();
 	
 	/**
 	 * @param numero
@@ -28,10 +31,42 @@ public class Client {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	public void addCompte(Compte c) {
+		getComptes().add(c);
+	}
+
+	public ArrayList<Compte> getComptes() {
+		return comptes;
+	}
 
 	@Override
 	public String toString() {
-		return "Client " + numero + " - " + nom;
+		String text = "Client " + numero + " - " + nom;
+		
+		if(getComptes() != null && !getComptes().isEmpty()) {
+			Iterator<Compte> iterator_compte = getComptes().iterator();
+			while(iterator_compte.hasNext()) {
+				Compte compte_client = iterator_compte.next();
+				text += "\nCompte n°" + compte_client.getnumero() + " - Solde " + compte_client.getsolde();
+			}
+		} else {
+			text += " pas de compte rattaché à ce client";
+		}
+		
+		return text;
+	}
+	
+	public static void main(String [] args){
+		Client c1 = new Client(1,"Martin");
+		System.out.println(c1);
+		Compte com1 = new Compte(10,c1);
+		Compte com2 = new Compte(20,c1);
+		c1.addCompte(com1);
+		c1.addCompte(com2);
+		System.out.println(c1);
+		com1.credit(500.0);
+		System.out.println(c1);
 	}
 	
 }
--- src/Compte.java
+++ src/Compte.java
@@ -25,17 +25,17 @@ public class Compte {
 	public String toString() {
 		return "Client " + numero + " - client " + client.getNom() + " - Solde " + solde;
 	}
+	public void credit(double somme) {
+		this.solde += somme;
+	}
+	
 	public static void main(String[] args) {
-		
-		
-			Client c1 = new Client(1,"Martin");
-			Client c2 = new Client(2,"Dupond");
-			Compte com1 = new Compte(10,c1);
-			Compte com2 = new Compte(20,c2);
-			System.out.println(com1);
-			System.out.println(com2);
-			
-		
+		Client c1 = new Client(1,"Martin");
+		Client c2 = new Client(2,"Dupond");
+		Compte com1 = new Compte(10,c1);
+		Compte com2 = new Compte(20,c2);
+		System.out.println(com1);
+		System.out.println(com2);
 	}
 	
 	
