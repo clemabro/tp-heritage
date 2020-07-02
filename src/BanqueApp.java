2bcfd6d2cffd4cd73fa2afc73ddc1ac30fb171de
justinbah <bahier.justin@gmail.com>
2020-07-02 20:24:58 +0200

    modif banquApp

--- src/BanqueApp.java
+++ src/BanqueApp.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 public class BanqueApp {
 
 	
-	private ArrayList<Client> client = new ArrayList<Client>();
+	private ArrayList<Client> client = new ArrayList<Client>(); //tableau de client
 	
 	
 	public void addClient(Client c) {
@@ -19,23 +19,20 @@ public class BanqueApp {
 	public static void main(String [] args){
 	
 		BanqueApp BanqueApp1 = new BanqueApp();
-		System.out.println(BanqueApp1);
+		System.out.println("Banque DuTresor");
 		
-		Client c1 = new Client(1,"Client1");
+		Client c1 = new Client(1,"Client1");//créer une ArrayList de Client
 		Client c2 = new Client(2,"Client2");
 		Client c3 = new Client(3,"Client3");
 		Client c4 = new Client(4,"Client4");
 		
-		BanqueApp1.addClient(c1);
-		BanqueApp1.addClient(c2);
+		BanqueApp1.addClient(c1);//ajouter 4 clients à la collection des clients de la banque (numéro 1 à 4 et nom								
+		BanqueApp1.addClient(c2);//"Client" suivi du numéro : Client1, Client2, etc.)
 		BanqueApp1.addClient(c3);
 		BanqueApp1.addClient(c4);
 		
 		
-		
-		if(BanqueApp1.getClient() != null && !BanqueApp1.getClient().isEmpty()) {
-		
-			Iterator<Client> iterator_client = BanqueApp1.getClient().iterator();
+			Iterator<Client> iterator_client = BanqueApp1.getClient().iterator();//lister les clients de la banque
 			while(iterator_client.hasNext()) {
 				Client client_banqueapp = iterator_client.next();
 				
@@ -43,26 +40,23 @@ public class BanqueApp {
 				System.out.println(client_banqueapp);
 				
 			}
-		}
-		else {
-			System.out.println(" pas de client rattaché cette banque");
-		}
 		
 		
 		
 		
 		
 		
-		
-		Iterator<Client> iterator_client = BanqueApp1.getClient().iterator();
-		while(iterator_client.hasNext()) {
+		iterator_client = BanqueApp1.getClient().iterator();
+		while(iterator_client.hasNext()) {//dans une boucle, parcourir les clients pour leur ajouter des comptes :
 			
-			Client client_banqueapp = iterator_client.next();
+			Client client_banqueapp = iterator_client.next();//récupérer le client
 			
-			CompteCourant com1 = new CompteCourant((client_banqueapp.getNumero()*(1000 + 1)),client_banqueapp);
-			CompteEpargne com2 = new CompteEpargne((client_banqueapp.getNumero()*(1000 + 1)),client_banqueapp,com1,(float)50);
+			CompteCourant com1 = new CompteCourant((client_banqueapp.getNumero()*(1000 + 1)),client_banqueapp);//créer un compte courant pour le client (numéro de compte : numéro du client *1000 + 1)
+			CompteEpargne com2 = new CompteEpargne((client_banqueapp.getNumero()*(1000 + 1)),client_banqueapp,com1,(float)50);//créer un compte épargne pour le client (numéro de compte : numéro du client *
+																																//1000 + 2, référence au compte courant (pour les transferts) et montant de
+																																//transfert de 50
 			
-			client_banqueapp.addCompte(com2);
+			client_banqueapp.addCompte(com2);//ajouter les 2 comptes au client
 			client_banqueapp.addCompte(com1);
 
 			
@@ -72,7 +66,7 @@ public class BanqueApp {
 		
 		
 		
-		 iterator_client = BanqueApp1.getClient().iterator();
+		 iterator_client = BanqueApp1.getClient().iterator();//lister les clients de la banque
 			while(iterator_client.hasNext()) {
 				Client client_banqueapp = iterator_client.next();
 				
