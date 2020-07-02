1e2474fc310f73980609e89f49338e54a44d5720
justinbah <bahier.justin@gmail.com>
2020-07-02 14:34:34 +0200

    class compte

---
+++ src/Compte.java
@@ -0,0 +1,43 @@
+
+public class Compte {
+
+	private long numero;
+	private Client client;
+	private float solde;
+	
+	public Compte(long numero, Client c) {
+		super();
+		this.numero = numero;
+		this.client=c;
+	}
+	public float getsolde()
+	{	
+		return solde;
+	}
+	public Client getClient()
+	{	
+		return client;
+	}
+	public long getnumero()
+	{	
+		return numero;
+	}
+	public String toString() {
+		return "Client " + numero + " - client" + client.getNom() + "- solde " + solde;
+	}
+	public static void main(String[] args) {
+		
+		
+			Client c1 = new Client(1,"Martin");
+			Client c2 = new Client(2,"Dupond");
+			Compte com1 = new Compte(10,c1);
+			Compte com2 = new Compte(20,c2);
+			System.out.println(com1);
+			System.out.println(com2);
+			
+		
+	}
+	
+	
+	
+}
